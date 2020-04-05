6923dd6ea337be365a24f7a5d1ba02ad2dd88524
rishav <rishav.21m@gmail.com>
2020-04-06 02:27:03 +0530

    changed class

--- index.js
+++ index.js
@@ -1,81 +1,99 @@
-let canvas=document.getElementById("canvas");
-let ctx=canvas.getContext('2d');
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 
 let WIDTH = canvas.width;
-let HEIGHT= canvas.height;
+let HEIGHT = canvas.height;
 
-class Vector {
-    constructor(x=WIDTH/2,y=HEIGHT/2){
-        this.x=x;
-        this.y=y;
-    }
-
-    add(v){
-        let ret = new Vector(this.x + v.x, this.y + v.y);
-        if (ret.x < 0)
-            ret.x = WIDTH,ret.y=Math.abs(ret.y-HEIGHT);
-        if (ret.x > WIDTH)
-            ret.x = 0,ret.y=Math.abs(ret.y-HEIGHT);
-        if (ret.y < 0)
-            ret.y = HEIGHT,ret.x=Math.abs(ret.x-WIDTH);
-        if (ret.y > HEIGHT)
-            ret.y = 0,ret.x=Math.abs(ret.x-WIDTH);
-        return ret; 
-    }
-
-    sub(v){
-        let ret = new Vector(this.x - v.x, this.y - v.y);
-        if (ret.x < 0)
-            ret.x = WIDTH;
-        if (ret.x > WIDTH)
-            ret.x = 0;
-        if (ret.y < 0)
-            ret.y = HEIGHT;
-        if (ret.y > HEIGHT)
-            ret.y = 0;
-        return ret;
-    }
-}
+let flocks=[];
 
 class Boid {
-    constructor(velocity=null,position=null){
-        if (velocity===null)
-            this.velocity=new Vector(2,1);
-        else 
-            this.velocity=velocity;
-        if (position===null)
-            this.position=new Vector();
-        else 
-            this.position=position;
-    }
+  constructor(velocity = null, position = null) {
+    if (velocity === null) {
+      let min = -4;
+      let max =-1;
+      let x = Math.floor(Math.random() * (max - min + 1) + min)+1;
+      let y = Math.floor(Math.random() * (max - min + 1) + min)+1;
+      this.velocity = new Victor(x, y);
+    } else this.velocity = velocity;
+
+    if (position === null) {
+      let min = 1;
+      let x = Math.floor(Math.random() * (WIDTH - min + 1) + min);
+      let y = Math.floor(Math.random() * (HEIGHT - min + 1) + min);
+      this.position = new Victor(x, y);
+    } else this.position = position;
+    this.acceleration = new Victor(0,0);
+    this.maxForce=1;
+    this.maxSpeed=4;
+  }
+
+  edges (){
+    if (this.position.x>WIDTH)
+      this.position.x=0;
+    else if (this.position.x<0)
+      this.position.x=WIDTH;
     
-    draw (){
-        ctx.beginPath();
-        ctx.arc(this.position.x,this.position.y,4,0,2*Math.PI);
-        ctx.stroke();
+    if (this.position.y>HEIGHT)
+      this.position.y=0;
+    else if (this.position.y<0)
+      this.position.y=HEIGHT;
+  }
+
+  align(boids){
+    let perceptionRadius = 60;
+    let steering = new Victor();
+    let total =0;
+    for (let other of boids ){
+      let d= (this.position.distance(other.position));
+      if (other!==this && d<perceptionRadius){
+        steering.add(other.position);
+        total++;
+      }
     }
-}
-
-let flock=[];
-let temp = new Boid();
-flock.push(temp);
-temp.draw();
-
-function drawAllBoids() {
-    ctx.clearRect(0,0,WIDTH,HEIGHT); 
-    for (let boid of flock) {
-        boid.draw();
+    if (total>0){
+      steering.x/=total;
+      steering.y/=total;
+      steering.subtract(this.velocity);
+      steering.limit(1,0.001);
     }
+    return steering; 
+
+  }
+  calculateAcceleration (){
+    let alignment=this.align(flocks);
+
+    this.acceleration.add(alignment); 
+  
+
+  }
+
+  update (){
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed,0.5);
+    this.acceleration=new Victor(0,0); 
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, 4, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
 }
 
-function moveAllBoids() {
-    for (let boid of flock){
-        boid.position=boid.position.add(boid.velocity);
-    }
+for (let i=0;i<40;i++){
+  let temp = new Boid();
+  flocks.push(temp);
 }
 
-function loop() {
-    drawAllBoids();
-    //move all boids
-    moveAllBoids();
+function loop (){
+  ctx.clearRect(0,0,WIDTH,HEIGHT);
+  for (let boid of flocks){
+    boid.draw(); 
+    //boid.calculateAcceleration();
+    boid.update(); 
+    boid.edges();
+  }
 }
+
+setInterval(loop,30);
